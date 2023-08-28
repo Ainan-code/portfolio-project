3362c6958db3d278ddfe2d03a274a3df4eae2066
Ainan <ainanhassan123@gmail.com>
2023-08-28 21:28:35 +0300

    footer in progress

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,10 +4,15 @@
 export default function Contact () {
     return(
         <div className="contact-section">
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsum unde quam! Odit nulla distinctio explicabo nostrum praesentium id autem velit, veritatis inventore doloribus molestias alias ut consequatur quibusdam labore!</div>
-            <div></div>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat obcaecati earum quisquam neque, nulla nihil animi recusandae tempore inventore facere culpa laborum, veniam fugiat atque dolor eveniet, rerum tenetur optio!</div>
-            <div></div>
+
+            <h3 className="contact-header">Contact me</h3>
+            <div className="contact-getintouch">Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic ipsum unde quam! Odit nulla </div>
+            <div className="contact-adress">Istanbul street 345 fjefn</div>
+            <div className="contact-phone">
+               <span>https://icons8.com/icon/Iw5aeMT37fzK/phone</span> 555-555-555</div>
+            <div className="contact-mail"> 
+                <span>https://icons8.com/icon/86840/mail</span>  fake.hassen@gmail.com
+            </div>
         </div>
     )
 }
